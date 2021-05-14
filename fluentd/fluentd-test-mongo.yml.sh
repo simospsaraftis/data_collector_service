7cc7a44156038995b0fd3a69d5ed193adf64ed3f
Simosps <cs141049@uniwa.gr>
2021-05-14 18:40:09 +0000

    Add

--- fluentd/fluentd-test-mongo.yml.sh
+++ fluentd/fluentd-test-mongo.yml.sh
@@ -4,12 +4,12 @@
 ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 ip6=$(/sbin/ip -o -6 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 
-echo "[service]" > /project/courses/fluentd/inventory.yml
-/project/bin/swarmlab-nmap >> /project/courses/fluentd/inventory.yml
+echo "[service]" > /data_collector_service/fluentd/inventory.yml
+/data_collector_service/bin/swarmlab-nmap >> /data_collector_service/fluentd/inventory.yml
 
 
 # include master or not
-echo $ip4 >> /project/courses/fluentd/inventory.yml
+echo $ip4 >> /data_collector_service/fluentd/inventory.yml
 
 
 ansible-playbook -u docker -i inventory.yml fluentd-test-mongo.yml  -f 5  --ask-pass --ask-become-pass
