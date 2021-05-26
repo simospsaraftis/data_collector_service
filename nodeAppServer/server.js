b15e8236e879dc5267b0fbcc5ca5ec824876e40f
Simosps <cs141049@uniwa.gr>
2021-05-26 21:13:50 +0000

    Add

--- nodeAppServer/server.js
+++ nodeAppServer/server.js
@@ -1,19 +1,38 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-var server = http.createServer(app);
-var io = require("socket.io")(server)
-var MongoClient = require('mongodb').MongoClient;
-var PORT = 8080;
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server)
+const mongoose = require('mongoose');
 
+mongoose.connect("mongodb://swarmlabmongo1:27017/");
 
-server.listen(PORT, () => {
-		console.log("HTTP server listening on port %s", PORT);
+const db = mongoose.connection;
+
+var PORT = 8000;
+
+db.on('error', console.error.bind(console, 'Connection Error:'));
+
+db.once('open', () => {
+	console.log("DB opened");
+	server.listen(PORT, () => {
+    console.log("HTTP server listening on port %s", PORT);
 });
 
+const taskCollection = db.collection('logs');
+      const changeStream = taskCollection.watch();
+        
+      changeStream.on('change', (change) => {
+      console.log(change);
+      });
+});
+
+
+/*
 app.get('/', function(req, res) {
 		res.sendFile(__dirname + '/index.html');
 });
+*/
 
 io.on('connection', (socket) => {
 		console.log("Client connected");
