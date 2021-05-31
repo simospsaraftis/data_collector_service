c429b5795df2304b53fe85da3c465fb71475646d
Simosps <cs141049@uniwa.gr>
2021-05-31 11:48:29 +0000

    Add

--- fluentd/fluentd.yml.sh
+++ fluentd/fluentd.yml.sh
@@ -3,13 +3,13 @@
 sudo apt update -y
 sudo apt install -y ansible sshpass
 
-sudo chown $(whoami) /data_collector_service/fluentd/inventory.yml
-sudo chmod +rwx /data_collector_service/fluentd/inventory.yml
+sudo chown $(whoami) /data_collector_service/fluentd/
+sudo chmod +rwx /data_collector_service/fluentd/
 
 sudo mkdir -p /home/docker/.ansible
 sudo chown docker.docker -R /home/docker
 
-sudo cp files/ansible.cfg /etc/ansible/ansible.cfg
+sudo cp ../ansible.cfg /etc/ansible/ansible.cfg
 
 ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 ip6=$(/sbin/ip -o -6 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
--- nodeAppServer/nodejs.yml.sh
+++ nodeAppServer/nodejs.yml.sh
@@ -3,26 +3,26 @@
 sudo apt update -y
 sudo apt install -y ansible sshpass
 
-sudo chown $(whoami) /data_collector_service/fluentd/inventory.yml
-sudo chmod +rwx /data_collector_service/fluentd/inventory.yml
+sudo chown $(whoami) /data_collector_service/nodeAppServer
+sudo chmod +rwx /data_collector_service/nodeAppServer
 
 sudo mkdir -p /home/docker/.ansible
 sudo chown docker.docker -R /home/docker
 
-sudo cp ../fluentd/files/ansible.cfg /etc/ansible/ansible.cfg
+sudo cp ../ansible.cfg /etc/ansible/ansible.cfg
 
 ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 ip6=$(/sbin/ip -o -6 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 
-echo "[service]" > /data_collector_service/fluentd/inventory.yml
-/data_collector_service/bin/swarmlab-nmap >> /data_collector_service/fluentd/inventory.yml
+echo "[service]" > /data_collector_service/nodeAppServer/inventory.yml
+/data_collector_service/bin/swarmlab-nmap >> /data_collector_service/nodeAppServer/inventory.yml
 
 
 # include master or not
-echo $ip4 >> /data_collector_service/fluentd/inventory.yml
+#echo $ip4 >> /data_collector_service/nodeAppServer/inventory.yml
 
 
-ansible-playbook -u docker -i /data_collector_service/fluentd/inventory.yml nodejs.yml  -f 5  --ask-pass --ask-become-pass
+ansible-playbook -u docker -i /data_collector_service/nodeAppServer/inventory.yml nodejs.yml  -f 5  --ask-pass --ask-become-pass
 # 1st make sudo without password
 # run with keys
 #ansible-playbook -u docker -i inventory.yml fluentd.yml  -f 5  --private-key=/home/docker/.ssh/id_rsa
