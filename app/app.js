aeab6f18433f49cb42c412bbdb1bb0a2c563cfb2
Simosps <cs141049@uniwa.gr>
2021-05-31 20:07:39 +0000

    Add

---
+++ app/app.js
@@ -0,0 +1,105 @@
+/*
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require("socket.io")(http);
+const MongoClient = require('mongodb').MongoClient;
+*/
+
+//var fs = require('fs');
+var express = require('express');
+var http = require('http');
+var app = express();
+//const cors = require('cors');
+const helmet = require('helmet');
+app.use(helmet());
+app.use(express.json());
+var serverPort = "8085";
+var server = http.createServer(app);
+const io = require("socket.io")(server);
+require('dotenv').config();
+var MongoClient = require('mongodb').MongoClient;
+
+/*
+var allowedOrigins = [ 
+      "http://localhost:8085"
+      ];
+*/
+
+/*
+app.use(cors({
+origin: function(origin, callback){    // allow requests with no origin 
+      if(!origin) 
+        return callback(null, true);    
+        if(allowedOrigins.indexOf(origin) === -1){
+          var msg = 'Not allowed';
+          return callback(new Error(msg), false);
+         }
+      return callback(null, true);
+    }
+}));
+*/
+
+/*
+const io = require("socket.io")(server, {
+   cors: {
+         origin: allowedOrigins,
+         //origin: [ "http://localhost:8085" ],
+         methods: ["GET", "POST"]
+   }
+});
+*/
+
+
+//------------Server Listen---------------//
+
+server.listen(serverPort, () => {
+		console.log("HTTP server listening on port %s", serverPort);
+});
+
+//----------------------------------------//
+
+
+//-----------Socket.IO-------------//
+
+io.on('connection', (socket) => {
+		console.log("Client connected");
+		socket.on('disconnect',() => {
+				console.log("Client disconnected");
+		});
+});
+
+
+const transmit = change => {
+		io.emit('change_msg',change);
+}
+
+//-------------------------------------------//
+
+
+//---------------CHangeStream---------------//
+
+//var mongoserver = JSON.parse(fs.readFileSync("/data_collector_service/app/config.json", "utf8"));
+
+var mongourl = "mongodb://"+process.env.MONGO_INITDB_ROOT_USERNAME+":"+process.env.MONGO_INITDB_ROOT_PASSWORD+"@"+process.env.MONGO_INITDB_NAME+":"+process.env.MONGO_INITDB_PORT+"/";
+
+MongoClient.connect(mongourl,{useNewUrlParser: true,useUnifiedTopology: true},(err, client) => {
+		if(err) 
+		{
+				console.log(err);
+		} 
+		else 
+		{
+			console.log("Connected to swarmlabmongo1");
+		  var db = client.db('app_swarmlab');
+		  const taskCollection = db.collection('logs');
+		  const changeStream = taskCollection.watch();
+
+		  changeStream.on('change', (change) => {
+      	console.log(change);
+				transmit(change);
+		});
+		};
+});
+
+
+//---------------------------------------------//
---
+++ app/nodejs.yml.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+sudo apt update -y
+sudo apt install -y ansible sshpass
+
+sudo chown $(whoami) /data_collector_service/app/inventory.yml
+sudo chmod +rwx /data_collector_service/app/inventory.yml
+
+sudo mkdir -p /home/docker/.ansible
+sudo chown docker.docker -R /home/docker
+
+sudo cp ../ansible.cfg /etc/ansible/ansible.cfg
+
+ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+ip6=$(/sbin/ip -o -6 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+
+echo "[service]" > /data_collector_service/app/inventory.yml
+/data_collector_service/bin/swarmlab-nmap >> /data_collector_service/app/inventory.yml
+
+
+# include master or not
+#echo $ip4 >> /data_collector_service/app/inventory.yml
+
+
+ansible-playbook -u docker -i /data_collector_service/app/inventory.yml nodejs.yml  -f 5  --ask-pass --ask-become-pass
+# 1st make sudo without password
+# run with keys
+#ansible-playbook -u docker -i inventory.yml nodejs.yml  -f 5  --private-key=/home/docker/.ssh/id_rsa
