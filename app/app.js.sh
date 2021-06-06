fbf9bc9c2ff33658e7595fc2b03581c1ea3a0211
Simosps <cs141049@uniwa.gr>
2021-06-06 13:01:11 +0000

    Add

--- app/app.js.sh
+++ app/app.js.sh
@@ -2,7 +2,7 @@ curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh \
 && sudo bash nodesource_setup.sh \
 && sudo apt-get install -y nodejs \
 && npm install \
-&& npm install socket.io \
+&& npm install socket.io
 && npm install socket.io-client \
 && npm install mongodb \
 && npm install express \
